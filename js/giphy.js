99a888980f4412e1afb1dbcde19bde7dcd9e5b20
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-11-25 13:52:04 -0500

    adds 10 random gifs on search

---
+++ js/giphy.js
@@ -0,0 +1,31 @@
+
+$(document).ready( function() {
+  $(".search").click(function(){
+    var keyword = $("input[name='keyword']").val();
+    var url = "http://api.giphy.com/v1/gifs/search?q="+keyword+"&api_key=dc6zaTOxFJmzC"
+    $.ajax({
+      url: url,
+      type: "GET",
+      dataType: "json"
+    }).done ( function(response){
+      console.log(response.data[0].url);
+      // call giphy function below to pull random gifs
+      giphy(response);
+    }).fail ( function (){
+      console.log("Failure");
+    }).always( function(){
+      console.log("Something's happening");
+    })
+  })
+})
+
+
+
+var giphy = function(response) {
+  for (var i=0;i<10;i++) {
+    // provides random number up to the length of the data array
+    var rand = Math.round(Math.random() * (response.data.length - 1));
+    // append image to body
+    $('body').append("<img src = '"+response.data[rand].images.fixed_height.url+"'>")
+  }
+}
